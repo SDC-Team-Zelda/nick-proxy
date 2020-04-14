ff52f377aecec6563a2d8367597bbb55f26e1bc2
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-14 18:54:40 -0400

    upda

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("newrelic");
 const express = require("express");
 const cors = require("cors");
-const port = 3000;
+const port = 5555;
 let app = express();
 app.use(express.json());
 app.use(express.static(__dirname + "/../public"));
