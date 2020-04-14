ff6cb3dfb48c6692cd86371da253ad505dc85832
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-14 19:00:52 -0400

    server update

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ const port = 5555;
 let app = express();
 app.use(express.json());
 app.use(express.static(__dirname + "/../public"));
-app.listen(port);
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
